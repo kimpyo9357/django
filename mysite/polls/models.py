4f56671fe3254a49c7377a3a1a2f4c5e549f817e
kimpyo9357 <kimpyo9357@naver.com>
2022-05-29 17:44:30 +0900

    Fixed Error part7

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,7 +1,9 @@
 import datetime
 
+from django.contrib import admin
 from django.db import models
 from django.utils import timezone
+
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
